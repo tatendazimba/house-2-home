6c86adc8c12867f88c34f4b0dba340a05b995c6a
tatenda <tatenda@poscloud.co.zw>
2020-01-23 11:40:53 +0200

    Bug fix

--- resources/views/partials/idea.blade.php
+++ resources/views/partials/idea.blade.php
@@ -26,17 +26,19 @@
                 </div>
 
                 <div class="col s12 offset-m1 m10 offset-l2 l8 ">
-                    @foreach($post->images[0]->prices as $i => $price)
-                        <div class="container center-align">
-                            <strong class="">{{ $i + 1 }}.</strong>
-                            <span>{{ $price->name }}</span> <strong class="">${{ $price->amount }}</strong>
-{{--                            <span class="right">--}}
-{{--                                <svg width="16" height="16" viewBox="0 0 16 16" class="icon">--}}
-{{--                                    <path id="defs-cart" d="M13.958 9.317l2-6a.971.971 0 0 0-.95-1.306V2H2.433L1.925.644a1 1 0 0 0-1.876.7l3.005 8.008a1 1 0 0 0 .938.648h9.016a1 1 0 0 0 .95-.683zM4.686 8l-1.5-4h10.436l-1.336 4h-7.6zm.308 6a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm9.016 0a2 2 0 1 1-2-2 2 2 0 0 1 2 2z"></path>--}}
-{{--                                </svg>--}}
-{{--                            </span>--}}
-                        </div>
-                    @endforeach
+                    @if(isset($post->images[0]))
+                        @foreach($post->images[0]->prices as $i => $price)
+                            <div class="container center-align">
+                                <strong class="">{{ $i + 1 }}.</strong>
+                                <span>{{ $price->name }}</span> <strong class="">${{ $price->amount }}</strong>
+    {{--                            <span class="right">--}}
+    {{--                                <svg width="16" height="16" viewBox="0 0 16 16" class="icon">--}}
+    {{--                                    <path id="defs-cart" d="M13.958 9.317l2-6a.971.971 0 0 0-.95-1.306V2H2.433L1.925.644a1 1 0 0 0-1.876.7l3.005 8.008a1 1 0 0 0 .938.648h9.016a1 1 0 0 0 .95-.683zM4.686 8l-1.5-4h10.436l-1.336 4h-7.6zm.308 6a2 2 0 1 1-2-2 2 2 0 0 1 2 2zm9.016 0a2 2 0 1 1-2-2 2 2 0 0 1 2 2z"></path>--}}
+    {{--                                </svg>--}}
+    {{--                            </span>--}}
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
 
                 <div class="col s12">
